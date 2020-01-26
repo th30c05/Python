6c6a42e54537ecf66b173f92b65110aaf04e790c
Arnau <Arnau@DESKTOP-B23JHJE>
2020-01-26 12:29:21 +0100

    Ejercicio 3 de 3 Enunciado: Crea un programa que pida tres números por teclado. El programa imprime en consola la media aritmética de los números introducidos.

---
+++ video11/ex3.py
@@ -0,0 +1,19 @@
+stop=False
+
+while stop==False:
+    print("Dame 3 numeros y hare la media aritmetica")
+    n1=int(input("Numero: "))
+    n2=int(input("Otro numero: "))
+    n3=int(input("Ultimo numero: "))
+    print("La media aritmetica es: "+str((n1+n2+n3)/3))
+    correcto=False
+    
+    while correcto==False:
+        otra=input("Quieres calcular otra media aritmetica? (Si // No) ")
+        if otra=="Si" or otra=="si" or otra=="sI" or otra=="SI" or otra=="S" or otra=="s":
+            correcto=True;stop=False
+        elif otra=="No" or otra=="no" or otra=="nO" or otra=="NO" or otra=="N" or otra=="n":
+            correcto=True;stop=True
+        else:
+            print("Solo escribe Si o No.")
+            correcto=False
